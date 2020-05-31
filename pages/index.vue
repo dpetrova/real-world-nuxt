af7fcb3c540dd2c39e6c65973c69f1eb77090c2f
dpetrova <daniella_petrova@abv.bg>
2020-05-31 16:26:48 +0300

    using vuex

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,7 @@
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import { mapState } from 'vuex' // <--- To map event
 
 export default {
   head() {
@@ -20,6 +21,9 @@ export default {
       title: 'Event Listing' // <-- For our title tag
     }
   },
+  computed: mapState({
+    events: state => state.events.events
+  }),
   //asyncData life hook is called every time before loading a page component
   //it will be called server-side once (on the first request to the Nuxt app) and client-side when navigating to further routes
   //this method receives the context as the first argument
@@ -36,17 +40,30 @@ export default {
   //   })
   // })
   // },
+
   //using ES6 destructuring the context object we can take out just the modules we need
-  async asyncData({ $axios, error }) {
+  // async asyncData({ $axios, error }) {
+  //   try {
+  //     // const response = await $axios.get('http://localhost:3000/events')
+  //     // return {
+  //     //   events: response.data
+  //     // }
+  //     const { data } = await EventService.getEvents()
+  //     return {
+  //       events: data
+  //     }
+  //   } catch (e) {
+  //     error({
+  //       statusCode: 503,
+  //       message: 'Unable to fetch events events at this time'
+  //     })
+  //   }
+  // },
+
+  //fetch page component hook works on the client & server-side to fill the store before rendering the page
+  async fetch({ store, error }) {
     try {
-      // const response = await $axios.get('http://localhost:3000/events')
-      // return {
-      //   events: response.data
-      // }
-      const { data } = await $axios.get('http://localhost:3000/events')
-      return {
-        events: data
-      }
+      await store.dispatch('events/fetchEvents')
     } catch (e) {
       error({
         statusCode: 503,
